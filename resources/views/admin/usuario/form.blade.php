a3e53ff91b535cb39630588031d565bfb0be1ec4
exit <use loja>
2018-08-16 16:14:04 -0300

    Acerto RegExp email cad; usuario

--- resources/views/admin/usuario/form.blade.php
+++ resources/views/admin/usuario/form.blade.php
@@ -45,7 +45,7 @@
              minlength="16" maxlength="60" required 
              onblur="validaEmail()"   
              placeholder="Informe um email do usuário" 
-             pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{1,2}$" 
+             pattern="[a-z0-9._%+-]+@[a-z0-9.]+\.[a-z]{1,3}$" 
              value="{{isset($usuario) ? $usuario->email 
                                      : old('email')}}">
     </div>    
