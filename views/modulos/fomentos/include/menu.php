6f6cc1eac24706e012489588dc6a4d1582aa5461
LoreleiGab <loreleigab@hotmail.com>
2020-02-19 18:48:03 -0300

    Menu para editais arquivados

--- views/modulos/fomentos/include/menu.php
+++ views/modulos/fomentos/include/menu.php
@@ -11,6 +11,12 @@
     </a>
 </li>
 <li class="nav-item">
+    <a href="<?= SERVERURL ?>fomentos/edital_arquivado_lista" class="nav-link" id="arquivado_edital">
+        <i class="fa fa-info nav-icon"></i>
+        <p>Editais arquivados</p>
+    </a>
+</li>
+<li class="nav-item">
     <a href="<?= SERVERURL ?>fomentos/exporta_inscritos" class="nav-link" id="exporta_inscritos">
         <i class="fa fa-info nav-icon"></i>
         <p>Exporta inscritos</p>
