3090a272f0f3659c8187fabbb5d4d85ac61d9d7c
Diego Vicente <diego.vdossantos@gmail.com>
2020-02-19 18:21:33 -0300

    config db capac

--- config/configAPP.php
+++ config/configAPP.php
@@ -1,10 +1,16 @@
 <?php
     define('SERVER', "127.0.0.1");
-    define('DB', "siscontrat");
-    define('USER', "root", true);
-    define('PASS', "", true);
 
-    define('SGDB', "mysql:host=".SERVER.";dbname=".DB);
+    define('DB1', "siscontrat");
+    define('USER1', "root", true);
+    define('PASS1', "", true);
+
+    define('DB2', "capac_new");
+    define('USER2', "root", true);
+    define('PASS2', "", true);
+
+    define('SGDB1', "mysql:host=".SERVER.";dbname=".DB1);
+    define('SGDB2', "mysql:host=".SERVER.";dbname=".DB2);
 
     define('METHOD', 'AES-256-CBC', true);
     define('SECRET_KEY', 'S3cr3t', true);
