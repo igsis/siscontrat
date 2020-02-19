cacb974609dd7a64248551b430365ab19ff92bdc
LoreleiGab <loreleigab@hotmail.com>
2020-02-19 18:02:02 -0300

    início

--- views/modulos/fomentos/inicio.php
+++ views/modulos/fomentos/inicio.php
@@ -1,5 +1,5 @@
 <?php
-require_once "./controllers/ProjetoController.php";
+/*require_once "./controllers/ProjetoController.php";
 require_once "./controllers/FomentoController.php";
 unset($_SESSION['projeto_s']);
 unset($_SESSION['origem_id_s']);
@@ -8,17 +8,17 @@ $fomentoObj = new FomentoController();
 $projetoObj = new ProjetoController();
 
 $nomeEdital = $fomentoObj->recuperaNomeEdital($_SESSION['edital_s']);
-$projetos = $projetoObj->listaProjetos();
+$projetos = $projetoObj->listaProjetos();*/
 ?>
 <!-- Content Header (Page header) -->
 <div class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-9">
-                <h1 class="m-0 text-dark">Projetos</h1>
+                <h1 class="m-0 text-dark">Hello, world!</h1>
             </div><!-- /.col -->
             <div class="col-sm-3">
-                <a href="<?= SERVERURL ?>fomentos/projeto_cadastro"><button class="btn btn-success btn-block">Adicionar</button></a>
+
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
@@ -33,71 +33,11 @@ $projetos = $projetoObj->listaProjetos();
                 <!-- Horizontal Form -->
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">Projetos Cadastrados para o edital: <?=$nomeEdital?></h3>
+                        <h3 class="card-title">Projetos Cadastrados para o edital:</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="tabela" class="table table-bordered table-striped">
-                            <thead>
-                            <tr>
-                                <th>Protocolo</th>
-                                <th>Instituição</th>
-                                <th>Valor do Projeto</th>
-                                <th>Data cadastro/envio</th>
-                                <th>Enviado</th>
-                                <th>Ação</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($projetos as $projeto):
-                                $enviado = $projeto->protocolo == null ? false : true?>
-                                <tr>
-                                    <td><?=$enviado ? $projeto->protocolo : "Envie o projeto para obter seu protocolo"?></td>
-                                    <td><?=$projeto->instituicao?></td>
-                                    <td><?=$fomentoObj->dinheiroParaBr($projeto->valor_projeto)?></td>
-                                    <td><?=$enviado ? $projetoObj->dataHora($projeto->data_inscricao) : "Projeto não enviado" ?></td>
-                                    <td><?=$enviado ? "Sim" : "Não"?></td>
-                                    <td>
-                                        <div class="row">
-                                            <?php if (!$enviado): ?>
-                                                <div class="col">
-                                                    <a href="<?= SERVERURL . "fomentos/projeto_cadastro&id=" . $projetoObj->encryption($projeto->id) ?>"
-                                                       class="btn btn-sm btn-primary"><i class="fas fa-edit"></i> Editar</a>
-                                                </div>
-                                                <div class="col">
-                                                    <form class="form-horizontal formulario-ajax" method="POST"
-                                                          action="<?= SERVERURL ?>ajax/projetoAjax.php" role="form"
-                                                          data-form="delete">
-                                                        <input type="hidden" name="_method" value="apagaProjeto">
-                                                        <input type="hidden" name="id" value="<?= $projeto->id ?>">
-                                                        <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i>
-                                                            Apagar
-                                                        </button>
-                                                        <div class="resposta-ajax"></div>
-                                                    </form>
-                                                </div>
-                                            <?php else: ?>
-                                                <div class="col">
-                                                    <a href="<?= SERVERURL . "pdf/resumo_fomento.php?id=" . $projetoObj->encryption($projeto->id) ?>"
-                                                       class="btn btn-sm btn-primary" target="_blank"><i class="fas fa-edit"></i> Visualizar</a>
-                                                </div>
-                                            <?php endif; ?>
-                                        </div>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                            <tfoot>
-                            <tr>
-                                <th>Protocolo</th>
-                                <th>Instituição</th>
-                                <th>Valor do Projeto</th>
-                                <th>Data cadastro/envio</th>
-                                <th>Enviado</th>
-                                <th>Ação</th>
-                            </tr>
-                            </tfoot>
-                        </table>
+
                     </div>
                     <!-- /.card-body -->
 
