25ae8d1210f5fee7800cb04e4871ecdc2e2d591c
Diego Vicente <diego.vdossantos@gmail.com>
2020-02-21 12:45:22 -0300

    Insere / Edita edital

--- controllers/FomentoController.php
+++ controllers/FomentoController.php
@@ -72,8 +72,72 @@ class FomentoController extends MainModel
         return DbModel::consultaSimples("SELECT id FROM $tabela WHERE $where",true)->rowCount();
     }
 
+    /**
+     * <p>Insere um novo edital no sistema CAPAC</p>
+     * @param array $post
+     * @return string
+     */
     public function insereEdital($post) {
-        //
+        unset ($post['_method']);
+
+        $dados = MainModel::limpaPost($post);
+
+        $dados['valor_max_projeto'] = MainModel::dinheiroDeBr($dados['valor_max_projeto']);
+        $dados['valor_edital'] = MainModel::dinheiroDeBr($dados['valor_edital']);
+        $dados['data_abertura'] = MainModel::dataHoraParaSQL($dados['data_abertura']);
+        $dados['data_encerramento'] = MainModel::dataHoraParaSQL($dados['data_encerramento']);
+
+        $insert = DbModel::insert('fom_editais', $dados, true);
+        if ($insert->rowCount() >= 1) {
+            $edital_id = DbModel::connection(true)->lastInsertId();
+            $alerta = [
+                'alerta' => 'sucesso',
+                'titulo' => 'Edital Cadastrado!',
+                'texto' => 'Dados cadastrados com sucesso!',
+                'tipo' => 'success',
+                'location' => SERVERURL . 'fomentos/edital_cadastro&id=' . MainModel::encryption($edital_id)
+            ];
+        } else {
+            $alerta = [
+                'alerta' => 'simples',
+                'titulo' => 'Oops! Algo deu Errado!',
+                'texto' => 'Falha ao salvar os dados no servidor, tente novamente mais tarde',
+                'tipo' => 'error',
+            ];
+        }
+        return MainModel::sweetAlert($alerta);
+    }
+
+    public function editaEdital($post) {
+        $edital_id = MainModel::decryption($post['id']);
+        unset($post['id']);
+        unset ($post['_method']);
+
+        $dados = MainModel::limpaPost($post);
+
+        $dados['valor_max_projeto'] = MainModel::dinheiroDeBr($dados['valor_max_projeto']);
+        $dados['valor_edital'] = MainModel::dinheiroDeBr($dados['valor_edital']);
+        $dados['data_abertura'] = MainModel::dataHoraParaSQL($dados['data_abertura']);
+        $dados['data_encerramento'] = MainModel::dataHoraParaSQL($dados['data_encerramento']);
+
+        $update = DbModel::update('fom_editais', $dados, $edital_id, true);
+        if ($update->rowCount() >= 1 || DbModel::connection()->errorCode() == 0) {
+            $alerta = [
+                'alerta' => 'sucesso',
+                'titulo' => 'Edital Atualizado!',
+                'texto' => 'Dados atualizados com sucesso!',
+                'tipo' => 'success',
+                'location' => SERVERURL . 'fomentos/edital_cadastro&id=' . MainModel::encryption($edital_id)
+            ];
+        } else {
+            $alerta = [
+                'alerta' => 'simples',
+                'titulo' => 'Oops! Algo deu Errado!',
+                'texto' => 'Falha ao salvar os dados no servidor, tente novamente mais tarde',
+                'tipo' => 'error',
+            ];
+        }
+        return MainModel::sweetAlert($alerta);
     }
 
     /** @TODO: Verificar se esta função é realmente necessária
