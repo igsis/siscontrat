5ffc54cd90e90fadfac604b7ea8aafc31826fdf9
LoreleiGab <loreleigab@hotmail.com>
2020-02-21 11:49:54 -0300

    Ícones do menu

--- views/modulos/fomentos/include/menu.php
+++ views/modulos/fomentos/include/menu.php
@@ -1,24 +1,24 @@
 <li class="nav-item">
     <a href="<?= SERVERURL ?>fomentos/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-info nav-icon"></i>
+        <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="<?= SERVERURL ?>fomentos/edital_lista" class="nav-link" id="edital_lista">
-        <i class="fa fa-info nav-icon"></i>
+        <i class="far fa-circle nav-icon"></i>
         <p>Lista editais</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="<?= SERVERURL ?>fomentos/edital_arquivado_lista" class="nav-link" id="edital_arquivado_lista">
-        <i class="fa fa-info nav-icon"></i>
+        <i class="far fa-circle nav-icon"></i>
         <p>Editais arquivados</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="<?= SERVERURL ?>fomentos/exporta_inscritos" class="nav-link" id="exporta_inscritos">
-        <i class="fa fa-info nav-icon"></i>
-        <p>Exporta inscritos</p>
+        <i class="far fa-circle nav-icon"></i>
+        <p>Gerenciar inscritos</p>
     </a>
 </li>
