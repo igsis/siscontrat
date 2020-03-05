649032351b3c1653c7da66f12c9f24c0a4bd1b45
Lucas Dauto <lucasdauto79@gmail.com>
2020-03-05 13:19:27 -0300

    Tipo edital

--- views/modulos/fomentos/edital_cadastro.php
+++ views/modulos/fomentos/edital_cadastro.php
@@ -30,13 +30,25 @@ $fomento = $fomentoObj->recuperaEdital($id);
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form class="form-horizontal formulario-ajax" method="POST" action="<?= SERVERURL ?>ajax/fomentoAjax.php" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
+                    <form class="form-horizontal formulario-ajax" method="POST"
+                          action="<?= SERVERURL ?>ajax/fomentoAjax.php" role="form"
+                          data-form="<?= ($id) ? "update" : "save" ?>">
                         <input type="hidden" name="_method" value="<?= ($id) ? "editar" : "cadastrar" ?>">
                         <input type="hidden" name="data_publicacao" value="<?= date('Y-m-d H:i:s') ?>">
                         <?php if ($id): ?>
                             <input type="hidden" name="id" id="edital_id" value="<?= $id ?>">
                         <?php endif; ?>
                         <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group">
+                                        <label>Tipo de Pessoa: *</label>
+                                        <select class="form-control" name="pessoa_tipos_id" id="pessoa_tipos_id"
+                                                required>
+                                            <option value="">Selecione uma opção...</option>
+                                            <?php $fomentoObj->geraOpcao('pessoa_tipos', $fomento->pessoa_tipos_id ?? "", false, false, true); ?>
+                                        </select>
+                                    </div>
+                                </div>
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
@@ -55,7 +67,8 @@ $fomento = $fomentoObj->recuperaEdital($id);
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="descricao">Descrição: *</label>
-                                    <textarea name="descricao" id="descricao" class="form-control" rows="5" required><?=$fomento->descricao ?? ""?></textarea>
+                                    <textarea name="descricao" id="descricao" class="form-control" rows="5"
+                                              required><?= $fomento->descricao ?? "" ?></textarea>
                                 </div>
                             </div>
                             <div class="row">
@@ -70,17 +83,20 @@ $fomento = $fomentoObj->recuperaEdital($id);
                                     <label for="valor_edital">Valor edital: *</label>
                                     <input type="text" class="form-control" id="dinheiro" name="valor_edital"
                                            onKeyPress="return(moeda(this,'.',',',event))"
-                                           value="<?= isset($fomento->valor_edital) ? $fomentoObj->dinheiroParaBr($fomento->valor_edital) : "" ?>" required>
+                                           value="<?= isset($fomento->valor_edital) ? $fomentoObj->dinheiroParaBr($fomento->valor_edital) : "" ?>"
+                                           required>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="data_abertura">Data de abertura: *</label>
-                                    <input type="text" class="form-control date-picker" id="data_abertura" name="data_abertura"
+                                    <input type="text" class="form-control date-picker" id="data_abertura"
+                                           name="data_abertura"
                                            value="<?= isset($fomento->data_abertura) ? $fomentoObj->dataHora($fomento->data_abertura) : "" ?>"
                                            required>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <label for="data_encerramento">Data de encerramento: *</label>
-                                    <input type="text" class="form-control date-picker" id="data_encerramento" name="data_encerramento"
+                                    <input type="text" class="form-control date-picker" id="data_encerramento"
+                                           name="data_encerramento"
                                            value="<?= isset($fomento->data_encerramento) ? $fomentoObj->dataHora($fomento->data_encerramento) : "" ?>"
                                            required>
                                 </div>
@@ -90,7 +106,8 @@ $fomento = $fomentoObj->recuperaEdital($id);
                         <!-- /.card-body -->
                         <div class="card-footer">
                             <?php if ($id): ?>
-                                <a href="<?=SERVERURL?>fomentos/edital_anexos&id=<?=$id?>" class="btn btn-danger">Anexos Solicitados</a>
+                                <a href="<?= SERVERURL ?>fomentos/edital_anexos&id=<?= $id ?>" class="btn btn-danger">Anexos
+                                    Solicitados</a>
                             <?php endif ?>
                             <button type="submit" class="btn btn-info float-right">Gravar</button>
                         </div>
