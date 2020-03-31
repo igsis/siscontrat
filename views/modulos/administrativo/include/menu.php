cc3cce7205f6c6bb7cf6f8165fd2a6b0abdaf7d8
LoreleiGab <loreleigab@hotmail.com>
2020-03-31 16:56:13 -0300

    conflito 2

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -10,3 +10,9 @@
         <p>Estou aqui para criar conflito</p>
     </a>
 </li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+        <i class="fa fa-home nav-icon"></i>
+        <p>Estou aqui para criar conflito de novo</p>
+    </a>
+</li>
