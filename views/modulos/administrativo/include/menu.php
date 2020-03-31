8e25e88939b256f8570250c63f0af1b760c14380
LoreleiGab <loreleigab@hotmail.com>
2020-03-31 17:03:01 -0300

    teste

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -1,18 +1,18 @@
 <li class="nav-item">
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>Home</p>
-    </a>
+<a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+<i class="fa fa-home nav-icon"></i>
+<p>Home</p>
+</a>
 </li>
 <li class="nav-item">
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>Estou aqui para criar conflito!!!!! AEEEEEE</p>
-    </a>
+<a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+<i class="fa fa-home nav-icon"></i>
+<p>123456</p>
+</a>
 </li>
 <li class="nav-item">
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>Estou aqui para criar conflito de novo</p>
-    </a>
+<a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+<i class="fa fa-home nav-icon"></i>
+<p>Estou aqui para criar conflito de novo</p>
+</a>
 </li>
