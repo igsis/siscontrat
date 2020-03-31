6a9729d4eb6fb7d48f8a46f37643e9abbda5e898
LoreleiGab <loreleigab@hotmail.com>
2020-03-31 16:58:39 -0300

    de novo

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -7,7 +7,7 @@
 <li class="nav-item">
     <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
         <i class="fa fa-home nav-icon"></i>
-        <p>Estou aqui para criar conflito</p>
+        <p>Estou aqui para criar conflito!!!!! AEEEEEE</p>
     </a>
 </li>
 <li class="nav-item">
