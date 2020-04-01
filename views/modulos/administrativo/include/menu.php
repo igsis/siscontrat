9d7df02e9cb28239ceaf151861d0158151aadfdb
Ajujuba <60891489+Ajujuba@users.noreply.github.com>
2020-04-01 17:06:56 -0300

    Teste Mural

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -2,10 +2,15 @@
     <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
         <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
-        <li>Atualizações</li>
+        
     </a>
 </li>
-
+<li class="nav-item">
+    <a href="administrativo/mural" class="nav-link" id="aprovar">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Mural</p>
+    </a>
+</li>
 <li class="nav-item">
     <a href="#" class="nav-link" id="aprovar">
         <i class="fa fa-circle nav-icon"></i>
