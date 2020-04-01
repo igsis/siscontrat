9725b65ca29abdcf5a1d9da0ac3a1fa1fae07147
Diego <diego.vdossantos@gmail.com>
2020-04-01 17:06:44 -0300

    iniciando mural

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -2,7 +2,13 @@
     <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
         <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
-        <li>Atualizações</li>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>administrativo/mural" class="nav-link" id="mural">
+        <i class="fa fa-home nav-icon"></i>
+        <p>Mural</p>
     </a>
 </li>
 
@@ -17,10 +23,10 @@
         <i class="far fa-circle nav-icon"></i>
         <p>Categoria</p>
     </a>
-</li><li class="nav-item">
+</li>
+<li class="nav-item">
     <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
         <i class="far fa-circle nav-icon"></i>
         <p>Gerenciar Módulos</p>
     </a>
 </li>
-<p>teste</p>
