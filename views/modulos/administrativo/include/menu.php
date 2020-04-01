cad21fe244e2cdd246a5a8b85edbcf6f3e982ff1
Rubens <ruuh.liima@gmail.com>
2020-04-01 16:43:02 -0300

    Revert "item 4 ( certo ) <3"
    This reverts commit 311691245c503e84c31fec9845ec5da52bb754e5.

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -3,5 +3,10 @@
         <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
         <li>Atualizações</li>
-        <li>Gerenciar Módulos</li>
     </a>
+</li><li class="nav-item">
+    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+        <i class="fa fa-circle-o"></i>
+        <p>Gerenciar Módulos</p>
+    </a>
+</li>
