7ce5c862905cb7ccea5a4ff4317e55a031313b04
Diego <diego.vdossantos@gmail.com>
2020-04-01 15:43:50 -0300

    ajuste

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -1,23 +1,6 @@
-<p>TESTESSS</p>
 <li class="nav-item">
     <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
         <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>123</p>
-    </a>
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>123456</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
-        <i class="fa fa-home nav-icon"></i>
-        <p>Estou aqui para criar conflito de novo</p>
     </a>
 </li>
