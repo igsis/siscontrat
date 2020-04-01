7a172d48c2c4f13f636910e98452910a08714217
maria-carolina <mariacarolina1701@outlook.com>
2020-04-01 16:31:39 -0300

    Categoria - item

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -4,3 +4,9 @@
         <p>Home</p>
     </a>
 </li>
+<li class="nav-item">
+    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+        <i class="far fa-circle nav-icon"></i>
+        <p>Categoria</p>
+    </a>
+</li>
