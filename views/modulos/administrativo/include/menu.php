080d5b1b61a87871e473fae3cb63d3ce748144a2
Rubens <ruuh.liima@gmail.com>
2020-04-01 16:30:21 -0300

    item 4

--- views/modulos/administrativo/include/menu.php
+++ views/modulos/administrativo/include/menu.php
@@ -3,4 +3,9 @@
         <i class="fa fa-home nav-icon"></i>
         <p>Home</p>
     </a>
+</li><li class="nav-item">
+    <a href="<?= SERVERURL ?>administrativo/inicio" class="nav-link" id="inicio">
+        <i class="fa fa-circle-o"></i>
+        <p>Gerenciar Módulos</p>
+    </a>
 </li>
