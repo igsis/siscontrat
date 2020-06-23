90b0556af2b873d6152675ccd7c05865424abd52
Diego <diego.vdossantos@gmail.com>
2020-06-23 16:46:46 -0300

    planilha fomentos pf

--- pdf/fomento_inscritos_aprovados.php
+++ pdf/fomento_inscritos_aprovados.php
@@ -15,6 +15,7 @@ $arqObj = new ArquivoController();
 
 $edital = $fomentoObj->recuperaEdital($id);
 $nomeEdital = $edital->titulo;
+$pessoaTipo = $edital->pessoa_tipos_id;
 $inscritos = $fomentoObj->listaInscritos($id);
 
 $linkStyle = [
@@ -27,14 +28,14 @@ $linkStyle = [
 // Podemos renomear o nome das planilha atual, lembrando que um único arquivo pode ter várias planilhas
 $objPHPExcel->getProperties()->setCreator("Sistema SisContrat");
 $objPHPExcel->getProperties()->setLastModifiedBy("Sistema SisContrat");
-$objPHPExcel->getProperties()->setTitle("Relatório de Inscritos");
-$objPHPExcel->getProperties()->setSubject("Relatório de Inscritos");
+$objPHPExcel->getProperties()->setTitle("Relatório de Inscritos Aprovados");
+$objPHPExcel->getProperties()->setSubject("Relatório de Inscritos Aprovados");
 $objPHPExcel->getProperties()->setDescription("Gerado automaticamente a partir do Sistema SisContrat");
 $objPHPExcel->getProperties()->setKeywords("office 2007 openxml php");
 $objPHPExcel->getProperties()->setCategory("Fomentos");
 
 $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A1:H1')
-    ->setCellValue("A1","Lista de Inscristos Edital - $nomeEdital");
+    ->setCellValue("A1","Lista de Inscristos Aprovados Edital - $nomeEdital");
 
 //Colorir o header
 $objPHPExcel->getActiveSheet()->getStyle("A1:H1")->applyFromArray
@@ -49,97 +50,9 @@ $objPHPExcel->getActiveSheet()->getStyle("A1:H1")->applyFromArray
     )
 );
 
-
-// Criamos as colunas
-$objPHPExcel->setActiveSheetIndex(0)
-    ->setCellValue("A2", "CNPJ")
-    ->setCellValue("B2", "Protocolo")
-    ->setCellValue("C2", "Responsável pelo núcleo artístico")
-    ->setCellValue("D2", "Representante legal da empresa")
-    ->setCellValue("E2", "Razão social / Proponente")
-    ->setCellValue("F2", "Valor do projeto")
-    ->setCellValue("G2", "Duração")
-    ->setCellValue("H2", "Arquivos");
-
-// Definimos o estilo da fonte
-$objPHPExcel->getActiveSheet()->getStyle('A2:H2')->getFont()->setBold(true);
-
-//Colorir a primeira linha
-$objPHPExcel->getActiveSheet()->getStyle('A2:H2')->applyFromArray
-(
-    array
-    (
-        'fill' => array
-        (
-            'type' => PHPExcel_Style_Fill::FILL_SOLID,
-            'color' => array('rgb' => 'E0EEEE')
-        ),
-    )
-);
-
-
-$cont = 3;
-
-foreach ($inscritos as $inscrito){
-    $a = "A" . $cont;
-    $b = "B" . $cont;
-    $c = "C" . $cont;
-    $d = "D" . $cont;
-    $e = "E" . $cont;
-    $f = "F" . $cont;
-    $g = "G" . $cont;
-    $h = "H" . $cont;
-
-    if($inscrito->pessoa_tipo_id == 1){
-        require_once "../controllers/PessoaFisicaController.php";
-        $pessoaFisicaObj = new PessoaFisicaController();
-        $pf = $pessoaFisicaObj->recuperaPessoaFisica($pessoaFisicaObj->encryption($inscrito->pessoa_fisica_id));
-        $proponente = $pf['nome'];
-        $documento = $pf['cpf'];
-        $nomeRep = "não aplicável";
-    } else{
-        require_once "../controllers/PessoaJuridicaController.php";
-        require_once "../controllers/RepresentanteController.php";
-        $pessoaJuridicaObj = new PessoaJuridicaController();
-        $repObj = new RepresentanteController();
-        $pj = $pessoaJuridicaObj->recuperaPessoaJuridica($pessoaJuridicaObj->encryption($inscrito->pessoa_juridica_id));
-        $rep = $repObj->recuperaRepresentante($pessoaJuridicaObj->encryption($pj['representante_legal1_id']))->fetch();
-        $proponente = $pj['razao_social'];
-        $documento = $pj['cnpj'];
-        $nomeRep = $rep['nome'];
-    }
-
-    $zip = SERVERURL."api/downloadInscritos.php?id=".$inscrito->id;
-
-    $objPHPExcel->getActiveSheet()->getStyle($c)->getAlignment()->setWrapText(true);
-    $objPHPExcel->getActiveSheet()->getStyle($f)->getNumberFormat()->setFormatCode("#,##0.00");
-
-    $objPHPExcel->setActiveSheetIndex(0)
-        ->setCellValue($a, $documento)
-        ->setCellValue($b, $inscrito->protocolo)
-        ->setCellValue($c, $inscrito->nucleo_artistico)
-        ->setCellValue($d, $nomeRep)
-        ->setCellValue($e, $proponente)
-        ->setCellValue($f, $inscrito->valor_projeto)
-        ->setCellValue($g, $inscrito->duracao)
-        ->setCellValue($h, 'download');
-    $objPHPExcel->getActiveSheet()->getCell($h)->getHyperlink()->setUrl($zip);
-    $objPHPExcel->getActiveSheet()->getCell($h)->getStyle()->applyFromArray($linkStyle);
-
-    $cont++;
-}
-
-// Renomeia a guia
-$objPHPExcel->getActiveSheet()->setTitle('Inscritos');
-
-$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
-$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
-$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(40);
-$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
-$objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(40);
-$objPHPExcel->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
-$objPHPExcel->getActiveSheet()->getColumnDimension('G')->setAutoSize(true);
-$objPHPExcel->getActiveSheet()->getColumnDimension('H')->setAutoSize(true);
+// Inicia Include
+    include_once "./includes/fomento_inscritos_aprovados_pf.php";
+// Fim Include
 
 $objPHPExcel->getActiveSheet()->getRowDimension(1)->setRowHeight(-1);
 
@@ -147,7 +60,7 @@ $objPHPExcel->setActiveSheetIndex(0);
 ob_end_clean();
 ob_start();
 
-$nome_arquivo = "inscritos_" . date("d-m-Y") . ".xls";
+$nome_arquivo = "inscritos_aprovados" . date("d-m-Y") . ".xls";
 
 
 // Cabeçalho do arquivo para ele baixar(Excel2007)
---
+++ pdf/includes/fomento_inscritos_aprovados_pf.php
@@ -0,0 +1,116 @@
+<?php
+
+// Criamos as colunas
+$objPHPExcel->setActiveSheetIndex(0)
+    ->setCellValue("A2", "Protocolo")
+    ->setCellValue("B2", "Nome do Projeto")
+    ->setCellValue("C2", "Nome do núcleo artístico/coletivo artístico")
+    ->setCellValue("D2", "Nome do representante do núcleo")
+    ->setCellValue("E2", "Nome do produtor independente")
+    ->setCellValue("F2", "Valor do projeto")
+    ->setCellValue("G2", "Duração")
+    ->setCellValue("H2", "Nome Completo")
+    ->setCellValue("I2", "CPF")
+    ->setCellValue("J2", "Nome do Coletivo/Grupo")
+    ->setCellValue("K2", "Data de Nascimento")
+    ->setCellValue("L2", "E-mail")
+    ->setCellValue("M2", "Telefone #1")
+    ->setCellValue("N2", "Telefone #2")
+    ->setCellValue("O2", "CEP")
+    ->setCellValue("P2", "Rua")
+    ->setCellValue("Q2", "Número")
+    ->setCellValue("R2", "Bairro")
+    ->setCellValue("S2", "Cidade")
+    ->setCellValue("T2", "Estado");
+
+// Definimos o estilo da fonte
+$objPHPExcel->getActiveSheet()->getStyle('A2:T2')->getFont()->setBold(true);
+
+//Colorir a primeira linha
+$objPHPExcel->getActiveSheet()->getStyle('A2:T2')->applyFromArray
+(
+    array
+    (
+        'fill' => array
+        (
+            'type' => PHPExcel_Style_Fill::FILL_SOLID,
+            'color' => array('rgb' => 'E0EEEE')
+        ),
+    )
+);
+
+
+$cont = 3;
+
+foreach ($inscritos as $inscrito){
+    $a = "A" . $cont;
+    $b = "B" . $cont;
+    $c = "C" . $cont;
+    $d = "D" . $cont;
+    $e = "E" . $cont;
+    $f = "F" . $cont;
+    $g = "G" . $cont;
+    $h = "H" . $cont;
+    $i = "I" . $cont;
+    $j = "J" . $cont;
+    $k = "K" . $cont;
+    $l = "L" . $cont;
+    $m = "M" . $cont;
+    $n = "N" . $cont;
+    $o = "O" . $cont;
+    $p = "P" . $cont;
+    $q = "Q" . $cont;
+    $r = "R" . $cont;
+    $s = "S" . $cont;
+    $t = "T" . $cont;
+
+    require_once "../controllers/PessoaFisicaController.php";
+    $pessoaFisicaObj = new PessoaFisicaController();
+    $pf = $pessoaFisicaObj->recuperaPessoaFisica($pessoaFisicaObj->encryption($inscrito->pessoa_fisica_id));
+    $proponente = $pf['nome'];
+    $documento = $pf['cpf'];
+    $nomeRep = "não aplicável";
+
+    $zip = SERVERURL."api/downloadInscritos.php?id=".$inscrito->id;
+
+    $objPHPExcel->getActiveSheet()->getStyle($c)->getAlignment()->setWrapText(true);
+    $objPHPExcel->getActiveSheet()->getStyle($f)->getNumberFormat()->setFormatCode("#,##0.00");
+
+    $objPHPExcel->setActiveSheetIndex(0)
+        ->setCellValue($a, $documento)
+        ->setCellValue($b, $inscrito->protocolo)
+        ->setCellValue($c, $inscrito->nucleo_artistico)
+        ->setCellValue($d, $nomeRep)
+        ->setCellValue($e, $proponente)
+        ->setCellValue($f, $inscrito->valor_projeto)
+        ->setCellValue($g, $inscrito->duracao)
+        ->setCellValue($h, 'download')
+        ->setCellValue($i, 'download')
+        ->setCellValue($j, 'download')
+        ->setCellValue($k, 'download')
+        ->setCellValue($l, 'download')
+        ->setCellValue($m, 'download')
+        ->setCellValue($n, 'download')
+        ->setCellValue($o, 'download')
+        ->setCellValue($p, 'download')
+        ->setCellValue($q, 'download')
+        ->setCellValue($r, 'download')
+        ->setCellValue($s, 'download')
+        ->setCellValue($t, 'download');
+    $objPHPExcel->getActiveSheet()->getCell($h)->getHyperlink()->setUrl($zip);
+    $objPHPExcel->getActiveSheet()->getCell($h)->getStyle()->applyFromArray($linkStyle);
+
+    $cont++;
+}
+
+// Renomeia a guia
+$objPHPExcel->getActiveSheet()->setTitle('Inscritos Aprovados');
+
+$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(40);
+$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
+$objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(40);
+$objPHPExcel->getActiveSheet()->getColumnDimension('F')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('G')->setAutoSize(true);
+$objPHPExcel->getActiveSheet()->getColumnDimension('H')->setAutoSize(true);
