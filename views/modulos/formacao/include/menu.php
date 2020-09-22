88561f3a08c80b8bdcad6c0dcee7a3901fa63283
Diego <diego.vdossantos@gmail.com>
2020-09-22 13:21:43 -0300

    ajuste menu

--- views/modulos/formacao/include/menu.php
+++ views/modulos/formacao/include/menu.php
@@ -4,11 +4,76 @@
         <p>Home</p>
     </a>
 </li>
-<li class="nav-item">
-    <a href="#" class="nav-link">
-        <i class="far fa-circle nav-icon"></i>
-        <p>Acesso Administrativo</p>
+<li class="nav-item has-treeview">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+            Acesso Administrativo
+            <i class="right fas fa-angle-left"></i>
+        </p>
     </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <a href="#" class="nav-link active">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Cargo</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Coordenadoria</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Programa</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Linguagem</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Projeto</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Subprefeitura</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Território</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Vigência</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Habilitar / Desabilitar CAPAC</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-dot-circle nav-icon"></i>
+                <p>Inscritos no CAPAC</p>
+            </a>
+        </li>
+    </ul>
 </li>
 <li class="nav-item">
     <a href="<?= SERVERURL ?>formacao/pf_lista" class="nav-link" id="pf_lista">
