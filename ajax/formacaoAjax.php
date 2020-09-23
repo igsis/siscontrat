824e19d42d64000a15fc56c64a0706d0f0486575
Diego <diego.vdossantos@gmail.com>
2020-09-23 12:25:15 -0300

    funções para administrativo do mod de Formação

--- ajax/formacaoAjax.php
+++ ajax/formacaoAjax.php
@@ -9,15 +9,12 @@ if (isset($_POST['_method'])) {
     $insForm = new FormacaoController();
 
     switch ($_POST['_method']) {
-        case "cadastrar":
-            echo $insForm->insereFormacao();
+        case "cadastrarPf":
+            // echo $insForm->insereFormacao();
             break;
-        case "editar":
-            echo $insForm->editaFormacao($_POST['id']);
+        case "editarPf":
+            // echo $insForm->editaFormacao($_POST['id']);
             break;
-//        case "enviar":
-//            echo $insForm->envioEvento($_POST['id'], $_POST['modulo']);
-//            break;
     }
 } else {
     include_once "../config/destroySession.php";
--- controllers/FormacaoController.php
+++ controllers/FormacaoController.php
@@ -1,13 +1,99 @@
 <?php
 if ($pedidoAjax) {
-    require_once "../models/ValidacaoModel.php";
-    require_once "../controllers/PessoaFisicaController.php";
+    require_once "../models/MainModel.php";
 } else {
-    require_once "./models/ValidacaoModel.php";
-    require_once "./controllers/PessoaFisicaController.php";
+    require_once "./models/MainModel.php";
 }
 
-class FormacaoController extends ValidacaoModel
+class FormacaoController extends MainModel
 {
-    //
+    public function listaCargos()
+    {
+        //
+    }
+
+    public function editaCargo($id)
+    {
+        //
+    }
+
+    public function apagaCargo($id)
+    {
+
+    }
+
+    public function listaCoordenadorias()
+    {
+        //
+    }
+
+    public function editaCoordenadoria($id)
+    {
+        //
+    }
+
+    public function apagaCoordenadoria($id)
+    {
+        //
+    }
+
+    public function listaProgramas()
+    {
+        //
+    }
+
+    public function editaPrograma($id)
+    {
+        //
+    }
+
+    public function apagaPrograma($id)
+    {
+        //
+    }
+
+    public function listaLinguagens()
+    {
+        //
+    }
+
+    public function editaLinguagem($id)
+    {
+        //
+    }
+
+    public function apagaLinguagem($id)
+    {
+        //
+    }
+
+    public function listaProjetos()
+    {
+        //
+    }
+
+    public function editaProjeto($id)
+    {
+        //
+    }
+
+    public function apagaProjeto($id)
+    {
+        //
+    }
+
+    public function listaTerritorios()
+    {
+        //
+    }
+    
+    public function editaTerritorio($id)
+    {
+        //
+    }
+    
+    public function apagaTerritorio($id)
+    {
+        //
+    }
 }
