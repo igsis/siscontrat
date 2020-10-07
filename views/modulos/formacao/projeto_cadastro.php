a0d9aa2bdfbb6a55bf8a6e40fff655a0cb9e7d68
Ana Julia Terra <ajtalves@hotmail.com>
2020-10-07 16:23:52 -0300

    task #12 - Ok

--- views/modulos/formacao/projeto_cadastro.php
+++ views/modulos/formacao/projeto_cadastro.php
@@ -29,50 +29,37 @@ $projeto = $projetoObj->recuperaProjeto($id);
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">Dados</h3>
-                    </div>
-                    <!-- /.card-header -->
+                    </div> <!-- /.card-header -->
                     <div class="card-body">
-                    <div class="row justify-content-center"> 
-                    </div>
-                    </div>
-                    <div class="row" >
-                        <div class="col-12">
-                        <div class="row mx-2">
+                        <div class="row">
                             <div class="col-12">
-                                <form class="form-horizontal formulario-ajax" action="<?= SERVERURL ?>ajax/formacaoAjax.php" 
-                                method="POST" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
+                                <form class="form-horizontal formulario-ajax" action="<?= SERVERURL ?>ajax/formacaoAjax.php" method="POST" role="form" data-form="<?= ($id) ? "update" : "save" ?>">
                                     <input type="hidden" name="_method" value="<?= ($id) ? "editarProjeto" : "cadastrarProjeto" ?>">
-                                    <?php if ($id): ?>
+                                    <?php if ($id) : ?>
                                         <input type="hidden" name="id" id="modulo_id" value="<?= $id ?>">
                                     <?php endif; ?>
                                     <div class="row">
                                         <div class="form-group col">
                                             <label for="sigla">Projeto: *</label>
-                                            <input type="text" class="form-control" id="projeto" name="projeto"
-                                                maxlength="25" value="<?= $projeto->projeto ?? "" ?>"
-                                                required>
-                                        </div>                                    
-                                    
+                                            <input type="text" class="form-control" id="projeto" name="projeto" maxlength="25" value="<?= $projeto->projeto ?? "" ?>" required>
+                                        </div>
+
                                     </div>
-                                    
-                                    <div class="card-footer">
-                                        <a href="<?= SERVERURL ?>formacao/projeto_lista">
-                                            <button type="button" class="btn btn-default pull-left">Voltar</button>
-                                        </a>
-                                        <button type="submit" name="cadastra" id="cadastra" class="btn btn-primary float-right">
-                                            Gravar
-                                        </button>
-                                    </div >
                                     <div class="resposta-ajax"></div>
-                                </form>
                             </div>
                         </div>
-                        </div>  
+                    </div><!-- /.card-body -->
+                    <div class="card-footer">
+                        <a href="<?= SERVERURL ?>formacao/projeto_lista">
+                            <button type="button" class="btn btn-default pull-left">Voltar</button>
+                        </a>
+                        <button type="submit" name="cadastra" id="cadastra" class="btn btn-primary float-right">
+                            Gravar
+                        </button>
                     </div>
-                </div>
-                <!-- /.card -->
+                    </form>
+                </div> <!-- /.card-info -->
             </div>
-        </div>
-        <!-- /.row -->
+        </div> <!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
