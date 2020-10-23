9a67bb0d8478e40a8a957cc03b4f6b867f3353f7
Diego <diego.vdossantos@gmail.com>
2020-10-23 15:55:47 -0300

    validação das datas da vigencia

--- views/modulos/formacao/vigencia_cadastro.php
+++ views/modulos/formacao/vigencia_cadastro.php
@@ -51,7 +51,7 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button type="submit" name="cadastra" id="cadastra" class="btn btn-primary float-right">
+                            <button type="submit" name="cadastraVigencia" id="cadastraVigencia" class="btn btn-primary float-right">
                                 Gravar
                             </button>
                         </div>
@@ -65,7 +65,7 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
                     ?>
                     <div class="card <?= (!$parcela_vigencia) ? "card-danger" : "card-info"?>">
                         <div class="card-header">
-                            <h3 class="card-title">Dados das Parcelas <?= (!$parcela_vigencia) ? "- <strong>Ainda não Cadastrados<strong>" : ""?></h3>
+                            <h3 class="card-title">Dados das Parcelas <?= (!$parcela_vigencia) ? '- <strong>Ainda não Cadastrados</strong>' : ''?></h3>
                         </div>
                         <form class="form-horizontal formulario-ajax" action="<?= SERVERURL ?>ajax/formacaoAjax.php"
                               method="POST" role="form" data-form="<?= $parcela_vigencia ? "update" : "save" ?>">
@@ -79,7 +79,7 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
                                     <?php if ($parcela_vigencia): ?>
                                         <input type="hidden" name="parcela_id[]" value="<?= $parcela_vigencia[$i]->id ?>">
                                     <?php endif ?>
-                                    <div class="row">
+                                    <div class="row linhaParcela">
                                         <div class="form-group col-md-2">
                                             <label for="parcela[]">Parcela:</label>
                                             <input type="number" readonly class="form-control" value="<?= $i + 1 ?>" name="numero_parcelas[]" id="parcela[]" required>
@@ -92,22 +92,25 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
 
                                         <div class="form-group col-md-2">
                                             <label for="data_inicio">Data inicial:</label>
-                                            <input type="date" name="data_inicio[]" class="form-control" id="data_inicio<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_inicio ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <input type="date" name="data_inicio[]" class="form-control validaDatas" id="data_inicio_<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_inicio ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <span id="data_inicio_<?= $i ?>-error" class="error invalid-feedback">Esta <strong>data inicial</strong> deve ser menor que a <strong>data de pagamento</strong> anterior</span>
                                         </div>
 
                                         <div class="form-group col-md-2">
                                             <label for="data_fim">Data final: </label>
-                                            <input type="date" name="data_fim[]" class="form-control" id="data_fim<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_fim ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <input type="date" name="data_fim[]" class="form-control validaDatas" id="data_fim_<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_fim ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <span id="data_fim_<?= $i ?>-error" class="error invalid-feedback">A <strong>data final</strong> deve ser maior que a <strong>data inicial</strong></span>
                                         </div>
 
                                         <div class="form-group col-md-2">
                                             <label for="data_pagamento">Data pagamento: </label>
-                                            <input type="date" name="data_pagamento[]" class="form-control" id="data_pagamento<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_pagamento ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <input type="date" name="data_pagamento[]" class="form-control validaDatas" id="data_pagamento_<?= $i ?>" value="<?= $parcela_vigencia[$i]->data_pagamento ?? "" ?>" placeholder="DD/MM/AAAA">
+                                            <span id="data_pagamento_<?= $i ?>-error" class="error invalid-feedback">A data de pagamento deve ser maior que a data final</span>
                                         </div>
 
                                         <div class="form-group col-md-2">
                                             <label for="carga[]">Carga horária: </label>
-                                            <input type="number" name="carga_horaria[]" class="form-control" id="carga<?= $i ?>" value="<?= $parcela_vigencia[$i]->carga_horaria ?? "" ?>" min="1">
+                                            <input type="number" name="carga_horaria[]" class="form-control validaDatas" id="carga_horaria_<?= $i ?>" value="<?= $parcela_vigencia[$i]->carga_horaria ?? "" ?>" min="1">
                                         </div>
                                     </div>
                                 <?php } ?>
@@ -116,7 +119,7 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
                                 <a href="<?= SERVERURL ?>formacao/vigencia_lista" class="btn btn-default pull-left">
                                     Voltar
                                 </a>
-                                <button type="submit" name="cadastra" id="cadastra" class="btn btn-primary float-right">
+                                <button type="submit" name="cadastraParcelas" id="cadastraParcelas" class="btn btn-primary float-right">
                                     Gravar
                                 </button>
                             </div>
@@ -128,3 +131,41 @@ $vigencia = $vigenciaObj->recuperaVigencia($id);
         </div>  <!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
+
+<script>
+    $('.validaDatas').blur(function () {
+        $('.linhaParcela').each(function (key) {
+            let data_inicio = new Date($('#data_inicio_' + key).val());
+            let data_fim = new Date($('#data_fim_' + key).val());
+            let data_pagamento = new Date($('#data_pagamento_' + key).val());
+
+            if (data_fim.getTime() <= data_inicio.getTime()) {
+                $('#data_fim_' + key).addClass('is-invalid');
+            } else {
+                $('#data_fim_' + key).removeClass('is-invalid');
+            }
+
+            if (data_pagamento.getTime() <= data_fim.getTime()) {
+                $('#data_pagamento_' + key).addClass('is-invalid');
+            } else {
+                $('#data_pagamento_' + key).removeClass('is-invalid');
+            }
+
+            if (key > 0) {
+                let data_pagamento_anterior = new Date($('#data_pagamento_' + (key-1)).val());
+
+                if (data_inicio.getTime() <= data_pagamento_anterior.getTime()) {
+                    $('#data_inicio_' + key).addClass('is-invalid');
+                } else {
+                    $('#data_inicio_' + key).removeClass('is-invalid');
+                }
+            }
+        });
+
+        if ($('.is-invalid').length) {
+            $('#cadastraParcelas').attr('disabled', true)
+        } else {
+            $('#cadastraParcelas').attr('disabled', false)
+        }
+    });
+</script>
