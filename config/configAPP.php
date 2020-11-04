ffa591427fcdd6546e19c8667510b11bb866ead9
Lorelei <loreleigab@hotmail.com>
2020-11-04 16:24:28 -0300

    Revert "Merge branch 'homologacao' into mod-formacao"
    This reverts commit c910a342f68013c8fba1e15ebc5b740c9ec3ad94.

--- config/configAPP.php
+++ config/configAPP.php
@@ -2,12 +2,12 @@
     define('SERVER', "127.0.0.1");
 
     define('DB1', "siscontrat");
-    define('USER1', "siscontrat", true);
-    define('PASS1', "siscontrat!@#", true);
+    define('USER1', "root", true);
+    define('PASS1', "", true);
 
     define('DB2', "capac_new");
-    define('USER2', "capacnew", true);
-    define('PASS2', "c1a2p3a4c5", true);
+    define('USER2', "root", true);
+    define('PASS2', "", true);
 
     define('SGDB1', "mysql:host=".SERVER.";dbname=".DB1);
     define('SGDB2', "mysql:host=".SERVER.";dbname=".DB2);
